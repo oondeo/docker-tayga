6dedf9c795eb44dbe036530b899c52f1c7d420a2
Amon-Ra <amon.raj@gmail.com>
2018-07-03 11:12:32 +0200

    add interface address

--- Dockerfile
+++ Dockerfile
@@ -7,10 +7,11 @@ ENV \
 	TAYGA_CONF_IPV4_ADDR=172.18.0.100 \
 	TAYGA_CONF_PREFIX=2001:db8:64:ff9b::/96 \
 	TAYGA_CONF_DYNAMIC_POOL=172.18.0.128/25 \
+    TAYGA_CONF_IPV6_ADDR=2001:db8:64:ff9b::1 \
     PUBLIC_IF=eth0 \
     PRIVATE_IF=eth1
 
-RUN apt-get update && apt-get install -y tayga && apt-get clean
+RUN apt-get update && apt-get install -y tayga iptables && apt-get clean
 
 ADD docker-entry.sh /
 RUN chmod +x /docker-entry.sh
--- docker-entry.sh
+++ docker-entry.sh
@@ -15,9 +15,13 @@ EOF
 # Setup Tayga networking
 tayga -c ${TAYGA_CONF_DIR}/tayga.conf --mktun
 ip link set nat64 up
+ip addr add ${TAYGA_CONF_IPV4_ADDR} dev nat64
+ip addr add ${TAYGA_CONF_IPV6_ADDR} dev nat64
 ip route add ${TAYGA_CONF_DYNAMIC_POOL} dev nat64
 ip route add ${TAYGA_CONF_PREFIX} dev nat64
 iptables -t nat -A POSTROUTING -o ${PUBLIC_IF} -j MASQUERADE
+iptables -A FORWARD -i ${PUBLIC_IF} -o nat64 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i nat64 -o ${PUBLIC_IF} -j ACCEPT
 iptables -A FORWARD -i ${PUBLIC_IF} -o nat64 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i nat64 -o ${PUBLIC_IF} -j ACCEPT
 
